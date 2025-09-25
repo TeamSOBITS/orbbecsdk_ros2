9a7b5176e33c4f9effebe8512ceb7c853f85659b
Keith Valentin <68671699+MrKeith99@users.noreply.github.com>
2025-09-25 16:40:27 +0900

    [FIX] do not set ros key here

--- install.sh
+++ install.sh
@@ -6,15 +6,19 @@ echo "╔══╣ Install: Orbbecsdk ROS2 (STARTING) ╠══╗"
 # Keep track of the current directory
 DIR=`pwd`
 
-
+# Download required dependencies
 sudo apt update
-
 sudo apt install -y \
     curl \
     gnupg \
     lsb-release \
     libgflags-dev \
-    nlohmann-json3-dev  \
+    nlohmann-json3-dev \
+    libdw-dev
+
+# Download ROS packages
+sudo apt update
+sudo apt install -y \
     ros-${ROS_DISTRO}-image-transport \
     ros-${ROS_DISTRO}-image-transport-plugins \
     ros-${ROS_DISTRO}-compressed-image-transport \
@@ -23,18 +27,14 @@ sudo apt install -y \
     ros-${ROS_DISTRO}-diagnostic-updater \
     ros-${ROS_DISTRO}-diagnostic-msgs \
     ros-${ROS_DISTRO}-statistics-msgs \
-    ros-${ROS_DISTRO}-backward-ros \
-    libdw-dev
-
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] \
-http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2.list'
-sudo apt update
+    ros-${ROS_DISTRO}-backward-ros
 
 sudo /lib/systemd/systemd-udevd --daemon
+
 cd orbbec_camera/scripts/
 sudo bash install_udev_rules.sh
-sudo udevadm control --reload-rules && sudo udevadm trigger
+sudo udevadm control --reload-rules
+sudo udevadm trigger
 
 # Go back to previous directory
 cd ${DIR}
